a5d6d110c5153d5a4bc4fde516fa8154aeb1b931
Vishal Lad <vishal.i.lad@accenture.com>
2019-08-09 15:38:46 +0530

    Updated the release files

--- release-regression.sh
+++ release-regression.sh
@@ -11,41 +11,45 @@ src=$2
 targ=$3
 
 
-
+# Step 1: Validate if paramters are pass appropriately
 if [ -z "$version" ] || [ -z "$src" ] || [ -z "$targ" ]; then
   echo "Please specify appropriate version, source branch and target branch"
   exit
 fi
 
 
-# Output
-# Get version from package.json
+
+# Step 2: Get version from package.json and display info 
 PKG_VERSION=$(node -pe "require('./package.json').version")
 echo "Current Application version is $PKG_VERSION"
 echo "Releasing version for regression is $version. Merging from $src -> $targ"
 echo "-------------------------------------------------------------------------"
 
-# Ensure working directory in version branch clean
+# Step 3: Ensure working directory in version branch clean
 git update-index -q --refresh
 if ! git diff-index --quiet HEAD --; then
   echo "Working directory not clean, please commit your changes first"
   exit
 fi
 
-# Checkout test branch and merge develop branch into test
+# Step 4: Checkout test branch and get latest code
 git checkout $targ
 git pull 
 
-#Ensure src branch is updated with master branch
-COMMIT_AHEAD_CNT = git rev-list --count master..develop
+# Step 4: Check if target branch is not ahead of source branch
+COMMIT_AHEAD_CNT = git rev-list --count $targ..$src
 if [ COMMIT_AHEAD_CNT -gt 0 ]; then
   echo "$src branch is $COMMITS_COUNT behind $targ. Please update $src branch with $targ branch"
   echo "Aborting"
   exit
 fi
-git merge $src --no-ff --no-edit
 
-# Revert the changes and exit if conflicts exists
+# Step 5: Merge develop branch into test and update the version
+git merge $src --no-ff --no-edit
+npm --no-git-tag-version $version 
+git push
+
+# Step 6: Revert the changes and exit if conflicts exists
 CONFLICTS=$(git ls-files -u | wc -l)
 echo "Conflicts $CONFLICTS"
 if [ "$CONFLICTS" -gt 0 ] ; then
@@ -56,7 +60,7 @@ if [ "$CONFLICTS" -gt 0 ] ; then
    exit 1
 fi
 
-# Checkout dev branch and merge master into dev (to ensure we have the version)
+# Step 7: Get changes from target branch to source to ensure we have the version.
 git checkout $src
 git merge $targ --no-ff --no-edit
 git push
